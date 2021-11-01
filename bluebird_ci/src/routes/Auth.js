a1d1a1ecce4072133bac3130b91c9dc4bc4a35ea
Sung-Minseok <sms8189@gmail.com>
2021-11-01 16:16:55 +0900

    로그인 페이지 수정

--- bluebird_ci/src/routes/Auth.js
+++ bluebird_ci/src/routes/Auth.js
@@ -1,5 +1,7 @@
 import React, { useCallback, useState } from "react";
 
+const inputStyles = {};
+
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -16,23 +18,18 @@ const Auth = () => {
 
     const onSubmit = (event) => {
         event.preventDefault();
-        if(newAccount){
-            
-        }else{
-
-        }
     };
 
     const toggleAccount = () => setNewAccount(prev => !prev);
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input type="text" name="email" placeholder="Email" required value={email} onChange={onChange} />
-                <input type="password" name="password" placeholder="Passoword" required value={password} onChange={onChange} />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+            <form onSubmit={onSubmit} className="container">
+                <input type="text" name="email" placeholder="Email" required value={email} onChange={onChange} className="authInput"/>
+                <input type="password" name="password" placeholder="Passoword" required value={password} onChange={onChange} className="authInput"/>
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} className="authInput authSubmit" />
             </form>
-            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"} </span>
+            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"} className="authSwitch" </span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
