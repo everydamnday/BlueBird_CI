17865954e366a58cce405ca72b62a22ac8bb92fa
yhs <hansprac@gmail.com>
2021-11-16 16:25:22 +0900

    front ver1.1 + env rev2

--- bluebird_ci/src/index.js
+++ bluebird_ci/src/index.js
@@ -8,9 +8,6 @@ dotenv.config();
 // console.log(process.env.REACT_APP_API_SERVER_URL);
 const { REACT_APP_API_SERVER_URL } = process.env;
 console.log(REACT_APP_API_SERVER_URL);
-console.log(ENV1);
-// console.log(`${ENV}`);
-// console.log($ENV);
 
 axios.defaults.baseURL = REACT_APP_API_SERVER_URL;
 // axios.defaults.baseURL = "http://localhost:3005";
