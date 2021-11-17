504fa1fa8f6e0ed5b8b3ec1c2e56a8c4bcadf280
yhs <hansprac@gmail.com>
2021-11-17 15:06:26 +0900

    front ver1.1 + config.js

--- bluebird_ci/src/index.js
+++ bluebird_ci/src/index.js
@@ -4,12 +4,13 @@ import App from "./App";
 import "./styles.css";
 import axios from "axios";
 import dotenv from "dotenv";
-dotenv.config();
+import { Config } from "./config";
+// dotenv.config();
 // console.log(process.env.REACT_APP_API_SERVER_URL);
-const { REACT_APP_API_SERVER_URL } = process.env;
-console.log(REACT_APP_API_SERVER_URL);
+// const { REACT_APP_API_SERVER_URL } = process.env;
+// console.log(REACT_APP_API_SERVER_URL);
 
-axios.defaults.baseURL = REACT_APP_API_SERVER_URL;
+axios.defaults.baseURL = Config();
 // axios.defaults.baseURL = "http://localhost:3005";
 
 ReactDOM.render(
